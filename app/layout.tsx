554fe79981cef9e8457a63abb790147f3db23800
Ojasva Tomar <ojasvatomar47@gmail.com>
2024-02-22 08:24:00 +0530

    Initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,15 @@
-// import Navbar from '@/components/Navbar'
+import Navbar from '@/components/Navbar'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter, Space_Grotesk } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
-const spaceGrotesk = Space_Grotesk({ 
-  subsets: ['latin'], 
+const spaceGrotesk = Space_Grotesk({
+  subsets: ['latin'],
   weight: ['300', '400', '500', '600', '700']
- })
+})
 
- // modifies SEO
+// modifies SEO
 export const metadata: Metadata = {
   title: 'Pricewise',
   description: 'Track product prices effortlessly and save money on your online shopping.',
@@ -24,7 +24,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <main className="max-w-10xl mx-auto">
-          {/* <Navbar /> */}
+          <Navbar />
           {children}
         </main>
       </body>
