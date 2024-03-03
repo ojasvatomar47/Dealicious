d6210264c4235ae10b6666e6bc91b355ef472771
Ojasva Tomar <ojasvatomar47@gmail.com>
2024-03-03 11:36:46 +0530

    Added the email functionality and individual pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 import HeroCarousel from "@/components/HeroCarousel"
 import Searchbar from "@/components/Searchbar"
 import Image from "next/image"
-// import { getAllProducts } from "@/lib/actions"
-// import ProductCard from "@/components/ProductCard"
+import { getAllProducts } from "@/lib/actions"
+import ProductCard from "@/components/ProductCard"
 
 const Home = async () => {
-  // const allProducts = await getAllProducts();
+  const allProducts = await getAllProducts();
 
   return (
     <>
@@ -42,9 +42,9 @@ const Home = async () => {
         <h2 className="section-text">Trending</h2>
 
         <div className="flex flex-wrap gap-x-8 gap-y-16">
-          {/* {allProducts?.map((product) => (
+          {allProducts?.map((product) => (
             <ProductCard key={product._id} product={product} />
-          ))} */}
+          ))}
         </div>
       </section>
     </>
--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -5,6 +5,8 @@ import { connectToDB } from "../mongoose";
 import { scrapeAmazonProduct } from "../scraper";
 import { getAveragePrice, getHighestPrice, getLowestPrice } from "../utils";
 import Product from "../models/product.model";
+import { generateEmailBody, sendEmail } from "@/nodemailer";
+import { User } from "@/types";
 
 export async function scrapeAndStoreProduct(productUrl: string) {
     if (!productUrl) return;
@@ -44,5 +46,71 @@ export async function scrapeAndStoreProduct(productUrl: string) {
         revalidatePath(`/products/${newProduct._id}`);
     } catch (error) {
         throw new Error(`Failed to create/update product: ${error}`);
+    }
+}
+
+export async function getProductById(productId: string) {
+    try {
+        connectToDB();
+
+        const product = await Product.findOne({ _id: productId });
+
+        if (!product) return null;
+
+        return product;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export async function getAllProducts() {
+    try {
+        connectToDB();
+
+        const products = await Product.find();
+
+        return products;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export async function getSimilarProducts(productId: string) {
+    try {
+        connectToDB();
+
+        const currentProduct = await Product.findById(productId);
+
+        if (!currentProduct) return null;
+
+        const similarProducts = await Product.find({
+            _id: { $ne: productId },
+        }).limit(3);
+
+        return similarProducts;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export async function addUserEmailToProduct(productId: string, userEmail: string) {
+    try {
+        const product = await Product.findById(productId);
+
+        if (!product) return;
+
+        const userExists = product.users.some((user: User) => user.email === userEmail);
+
+        if (!userExists) {
+            product.users.push({ email: userEmail });
+
+            await product.save();
+
+            const emailContent = await generateEmailBody(product, "WELCOME");
+
+            await sendEmail(emailContent, [userEmail]);
+        }
+    } catch (error) {
+        console.log(error);
     }
 }
